92d191e931ab159dcca6e7cbe43a4cd208291619
AndreyFernandes-NP <andreyfernandes1361@gmail.com>
2023-05-30 18:29:12 -0300

    Commit na Questão 1

---
+++ Est_AVA2/Q1/Main.c
@@ -0,0 +1,28 @@
+﻿// Libs
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <stdbool.h>
+#include "funcoes.h"
+
+// Funções Protótipos
+
+bool Vetor();
+
+// Int Main
+
+int main(void) {
+  setlocale(LC_ALL, "Portuguese");
+  bool loop = true;
+  
+  while(loop){
+    loop = Vetor();
+  }
+
+  printf("\n-- Fim do Programa --\n");
+  
+  return 0;
+  
+}
